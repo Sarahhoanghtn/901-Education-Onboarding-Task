9208184862b3770341dcb24a2818b82a1bbccccd
Sarah Hoang <nhuhoang.sarah@gmail.com>
2026-09-17 14:44:05 +0700

    complete stage 4: enable team clicks and states

--- 901-onboarding/src/TeamCard.tsx
+++ 901-onboarding/src/TeamCard.tsx
@@ -1,10 +1,14 @@
 import type { Team } from './types'
 
-export default function TeamCard ({team}: {team: Team}) {
+export default function TeamCard ({team, isSelected, onSelect}: {team: Team, isSelected: boolean, onSelect: () => void}) {
     return (
-        <div>
+        <button onClick={onSelect}
+        style= {{
+            border: isSelected ? '3px solid blue' : '1px solid gray',
+            background: isSelected ? 'darkblue' : 'gray'
+        }}>
             <h1>{team.teamName}</h1>
             <h3>School: {team.school}</h3>
-        </div>
+        </button>
     );
 }
--- 901-onboarding/src/TeamList.tsx
+++ 901-onboarding/src/TeamList.tsx
@@ -1,14 +1,30 @@
 import type { Team } from './types'
+import { useState } from 'react'
+import TeamCard from './TeamCard'
 
 export default function TeamList({teams}: {teams: Team[]}) {
+    const [selectedTeamId, setSelectedTeamId] = useState<string | null>(null)
+    
+    function selectTeamClick(teamId: string) {
+        setSelectedTeamId(teamId);
+    }
+    const selectedTeam = teams.find(t => t.id === selectedTeamId);
+    const selectedLabel = selectedTeam ? selectedTeam.teamName : "none";
+    
     if (teams.length == 0) {
         return <p>No teams yet.</p>
     }
     const cards = teams.map(currTeam =>
+        <TeamCard 
+        key = {currTeam.id}
+        team={currTeam}
+        isSelected = {currTeam.id === selectedTeamId}
+        onSelect = {() => selectTeamClick(currTeam.id)} />
+    );
+    return (
         <div>
-            <h1>{currTeam.teamName}</h1>
-            <h3>School: {currTeam.school}</h3>
+            <p>Selected: {selectedLabel}</p>
+            <ul>{cards}</ul>
         </div>
     );
-    return <ul>{cards}</ul>;
 }
